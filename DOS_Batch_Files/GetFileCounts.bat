935cb74a33f92160041cb573eab13df8de60d2d6
unknown <pjb36@JRENNA-T3610-01.uanet.edu>
2015-04-15 22:08:22 -0400

    Added sorting back to the OFB files, renamed the main OFB file with '_Template', fixed GetFileCounts.bat to work with the current directory

--- DOS_Batch_Files/GetFileCounts.bat
+++ DOS_Batch_Files/GetFileCounts.bat
@@ -4,19 +4,25 @@ CLS
 
 SETLOCAL EnableDelayedExpansion
 
-CHDIR "..\..\CORDs MEA data"
-DEL FileCounts.txt
+REM Delete the previously generated FileCounts.txt file
+REM Don't output "Couldn't find file" if there wasn't one previously generated
+IF EXIST "FileCounts.csv" (
+	DEL "FileCounts.csv"
+)
 
+REM Store counts of MCD, PLX, and TXT files into FileCounts.txt
+ECHO Displaying counts of MCD, PLX, and TXT files in the current directory and all its subdirectories 
 FOR /r /d %%d IN (*) DO (
 	SET mcdCount=0
 	SET plxCount=0
 	SET txtCount=0
 	FOR %%f IN ("%%~fd\*.mcd") DO set /a mcdCount+=1
-	FOR %%f IN ("%%~fd\*-01.plx") DO set /a plxCount+=1
-	FOR %%f IN ("%%~fd\*-01.txt") DO set /a txtCount+=1
-	ECHO %%~fd,!mcdCount!,!plxCount!,!txtCount!>> ../Code/DOS_Batch_Files/FileCounts.txt
+	FOR %%f IN ("%%~fd\*.plx") DO set /a plxCount+=1
+	FOR %%f IN ("%%~fd\*.txt") DO set /a txtCount+=1
+	ECHO %%~fd,!mcdCount!,!plxCount!,!txtCount!>> FileCounts.csv
 )
 
+REM Deallocate environment variables
 SET mcdCount=
 SET plxCount=
 SET txtCount=
--- DOS_Batch_Files/WfArtifactsExport.create.bat
+++ DOS_Batch_Files/WfArtifactsExport.create.bat
@@ -1,10 +1,11 @@
-@echo off
+@ECHO off
 CLS
 SETLOCAL EnableDelayedExpansion
 
 REM Get the rootPath from the user
-ECHO Enter the fully qualified path to the directory containing .mcd files
-ECHO Note that .mcd files in all subdirectories will also be processed
+ECHO Enter the fully qualified path to a directory that
+ECHO does not contain MCD files, but whose subdirectories do.
+ECHO OFB files will be generated for all subdirectories.
 SET /p rootPath=">"
 
 REM If that path exists, continue
@@ -16,7 +17,7 @@ IF NOT EXIST "!rootPath!" (
 	
 	REM Get the rootPath from the user
 	ECHO.
-	ECHO Enter the fully qualified path to the directory where you wish to place the generated .ofb files
+	ECHO Enter the fully qualified path to a directory where you wish to place generated OFB files
 	SET /p destPath=">"
 	
 	REM If that path exists, continue
@@ -25,7 +26,7 @@ IF NOT EXIST "!rootPath!" (
 	) ELSE (	
 		REM Delete all previous .plx files if the user wishes
 		ECHO.
-		ECHO Delete previous .plx files from all subdirectories also? ^(y/n^)
+		ECHO Delete previous PLX files from all subdirectories also? ^(y/n^)
 		SET /p deleteMCDs=">"
 		IF !deleteMCDs!==y DEL /s "*.plx"
 
@@ -36,35 +37,56 @@ IF NOT EXIST "!rootPath!" (
 		)
 
 		REM For each subdirectory in the rootPath
-		SET count=0
+		SET count=1
 		ECHO.
 		FOR /r /d %%D IN (*) DO (
 			REM If it contains at least one .mcd file
-			IF EXIST "%%~fD\*.mcd" (
-				SET /a count+=1
-				
-				REM Create the .ofb file named like "count_subdirectoryName.ofb"  (the spaces after DetectSigmas and DetectDead are necessary for some reason)
+			IF EXIST "%%~fD\*.mcd" (						
+				REM Create the .ofb file named like "count_subdirectoryName.ofb"
+				REM Spaces before >> operators are only necessary sometimes for whatever reason
+				ECHO // Work with all .MCD files in the Data directory>> !count!_%%~nD.ofb
+				ECHO.>> !count!_%%~nD.ofb
+				ECHO // Queue all .MCD files in the Data directory>> !count!_%%~nD.ofb
 				ECHO Dir %%~fD\*.mcd>> !count!_%%~nD.ofb
 				ECHO Set DetectSigmas 5 >> !count!_%%~nD.ofb
 				ECHO Set DetectNPW 50>> !count!_%%~nD.ofb
 				ECHO Set DetectNPre 10>> !count!_%%~nD.ofb
 				ECHO Set DetectDead 0 >> !count!_%%~nD.ofb
 				ECHO ForEachChannel Detect>> !count!_%%~nD.ofb
+				ECHO.>> !count!_%%~nD.ofb
+				ECHO // Remove artifacts>> !count!_%%~nD.ofb
 				ECHO Set ArtifactWidth 10>> !count!_%%~nD.ofb
 				ECHO Set ArtifactPercentage 70>> !count!_%%~nD.ofb
 				ECHO ForEachFile InvalidateArtifactsAfter>> !count!_%%~nD.ofb
+				ECHO.>> !count!_%%~nD.ofb
+				ECHO // Export unsorted timestamps to .PLX files>> !count!_%%~nD.ofb
 				ECHO Set SaveCont 0 >> !count!_%%~nD.ofb
 				ECHO ForEachFile ExportToPlx>> !count!_%%~nD.ofb
+				ECHO.>> !count!_%%~nD.ofb
+				ECHO // Run T-Distribution E-M sorting on all channels, using principle components 1-3 as features>> !count!_%%~nD.ofb
+				ECHO Set FeatureX 0 >> !count!_%%~nD.ofb
+				ECHO Set FeatureY 1 >> !count!_%%~nD.ofb
+				ECHO Set FeatureZ 2 >> !count!_%%~nD.ofb
+				ECHO ForEachChannel TDist3d>> !count!_%%~nD.ofb
+				ECHO.>> !count!_%%~nD.ofb
+				ECHO // Export sorted timestamps to new .PLX files>> !count!_%%~nD.ofb
+				ECHO Set SaveCont 0 >> !count!_%%~nD.ofb
+				ECHO ForEachFile ExportToPlx>> !count!_%%~nD.ofb
+				ECHO.>> !count!_%%~nD.ofb
 				ECHO Process>> !count!_%%~nD.ofb
 				
+				REM Increase the file count and show the fileName just processed on the console
 				ECHO Created !count!_%%~nD.ofb
+				SET /a count+=1
 			)
 		)
 
 		REM Move all .ofb files to the appropriate Code folder
-		FOR /r %%F in ("*.ofb") DO move "%%~fF" "!destPath!"
+		FOR /r %%F in ("*.ofb") DO (
+			MOVE "%%~fF" "!destPath!"
+		)
 		ECHO.
-		ECHO All .ofb files are now in the directory "!destPath!"
+		ECHO All OFB files are now in the directory "!destPath!"
 		
 	)
 )
@@ -77,4 +99,4 @@ SET count=
 
 PAUSE
 ENDLOCAL
-@echo on
+@ECHO on
