669e47278344ff2f9dbdddcdcadf8e0bf0aa9fb9
Daniel Vicarel <shundra8820@gmail.com>
2016-04-15 03:38:30 -0400

    Improved the ModuleLoader ribbon layout, and added some error handling to the import code

--- ModuleLoader/ModuleLoaderVSTO/MainRibbon.Designer.cs
+++ ModuleLoader/ModuleLoaderVSTO/MainRibbon.Designer.cs
@@ -29,70 +29,185 @@
         /// </summary>
         private void InitializeComponent() {
             this.ModLoaderTab = this.Factory.CreateRibbonTab();
-            this.MainGrp = this.Factory.CreateRibbonGroup();
+            this.ImportGrp = this.Factory.CreateRibbonGroup();
+            this.ImportModulesDrop = this.Factory.CreateRibbonDropDown();
+            this.ImportClassesDrop = this.Factory.CreateRibbonDropDown();
+            this.ImportFormsDrop = this.Factory.CreateRibbonDropDown();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.ExportGrp = this.Factory.CreateRibbonGroup();
+            this.ExportModulesDrop = this.Factory.CreateRibbonDropDown();
+            this.ExportClassesDrop = this.Factory.CreateRibbonDropDown();
+            this.ExportFormsDrop = this.Factory.CreateRibbonDropDown();
+            this.ImportAllBtn = this.Factory.CreateRibbonButton();
             this.BrowseBtn = this.Factory.CreateRibbonButton();
-            this.ModulesDrop = this.Factory.CreateRibbonDropDown();
-            this.ClassesDrop = this.Factory.CreateRibbonDropDown();
-            this.FormsDrop = this.Factory.CreateRibbonDropDown();
+            this.ExportAllBtn = this.Factory.CreateRibbonButton();
+            this.RefreshGrp = this.Factory.CreateRibbonGroup();
+            this.RefreshBtn = this.Factory.CreateRibbonButton();
             this.ModLoaderTab.SuspendLayout();
-            this.MainGrp.SuspendLayout();
+            this.ImportGrp.SuspendLayout();
+            this.ExportGrp.SuspendLayout();
+            this.RefreshGrp.SuspendLayout();
             this.SuspendLayout();
             // 
             // ModLoaderTab
             // 
-            this.ModLoaderTab.Groups.Add(this.MainGrp);
+            this.ModLoaderTab.Groups.Add(this.ImportGrp);
+            this.ModLoaderTab.Groups.Add(this.ExportGrp);
+            this.ModLoaderTab.Groups.Add(this.RefreshGrp);
             this.ModLoaderTab.Label = "Module Loader";
             this.ModLoaderTab.Name = "ModLoaderTab";
             // 
-            // MainGrp
+            // ImportGrp
             // 
-            this.MainGrp.Items.Add(this.ModulesDrop);
-            this.MainGrp.Items.Add(this.ClassesDrop);
-            this.MainGrp.Items.Add(this.FormsDrop);
-            this.MainGrp.Items.Add(this.BrowseBtn);
-            this.MainGrp.Label = "Load";
-            this.MainGrp.Name = "MainGrp";
+            this.ImportGrp.Items.Add(this.ImportAllBtn);
+            this.ImportGrp.Items.Add(this.ImportModulesDrop);
+            this.ImportGrp.Items.Add(this.ImportClassesDrop);
+            this.ImportGrp.Items.Add(this.ImportFormsDrop);
+            this.ImportGrp.Items.Add(this.separator1);
+            this.ImportGrp.Items.Add(this.BrowseBtn);
+            this.ImportGrp.Label = "Import";
+            this.ImportGrp.Name = "ImportGrp";
+            // 
+            // ImportModulesDrop
+            // 
+            this.ImportModulesDrop.Label = "Modules";
+            this.ImportModulesDrop.Name = "ImportModulesDrop";
+            this.ImportModulesDrop.OfficeImageId = "ModuleInsert";
+            this.ImportModulesDrop.ScreenTip = "Import Module";
+            this.ImportModulesDrop.ShowImage = true;
+            this.ImportModulesDrop.ShowItemImage = false;
+            this.ImportModulesDrop.SizeString = "The really long name of a module";
+            this.ImportModulesDrop.SuperTip = "Choose one of the Basic Modules from the same directory as this workbook to impor" +
+    "t.";
+            this.ImportModulesDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ImportModulesDrop_SelectionChanged);
+            // 
+            // ImportClassesDrop
+            // 
+            this.ImportClassesDrop.Label = "Classes";
+            this.ImportClassesDrop.Name = "ImportClassesDrop";
+            this.ImportClassesDrop.OfficeImageId = "ComAddInsDialog";
+            this.ImportClassesDrop.ScreenTip = "Import Class";
+            this.ImportClassesDrop.ShowImage = true;
+            this.ImportClassesDrop.ShowItemImage = false;
+            this.ImportClassesDrop.SizeString = "The really long name of a module";
+            this.ImportClassesDrop.SuperTip = "Choose one of the Class Modules from the same directory as this workbook to impor" +
+    "t.";
+            this.ImportClassesDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ImportClassesDrop_SelectionChanged);
+            // 
+            // ImportFormsDrop
+            // 
+            this.ImportFormsDrop.Label = "Forms";
+            this.ImportFormsDrop.Name = "ImportFormsDrop";
+            this.ImportFormsDrop.OfficeImageId = "FormPublish";
+            this.ImportFormsDrop.ScreenTip = "Import Form";
+            this.ImportFormsDrop.ShowImage = true;
+            this.ImportFormsDrop.ShowItemImage = false;
+            this.ImportFormsDrop.SizeString = "The really long name of a module";
+            this.ImportFormsDrop.SuperTip = "Choose one of the Form Modules from the same directory as this workbook to import" +
+    ".";
+            this.ImportFormsDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ImportFormsDrop_SelectionChanged);
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // ExportGrp
+            // 
+            this.ExportGrp.Items.Add(this.ExportAllBtn);
+            this.ExportGrp.Items.Add(this.ExportModulesDrop);
+            this.ExportGrp.Items.Add(this.ExportClassesDrop);
+            this.ExportGrp.Items.Add(this.ExportFormsDrop);
+            this.ExportGrp.Label = "Export";
+            this.ExportGrp.Name = "ExportGrp";
+            // 
+            // ExportModulesDrop
+            // 
+            this.ExportModulesDrop.Label = "Modules";
+            this.ExportModulesDrop.Name = "ExportModulesDrop";
+            this.ExportModulesDrop.OfficeImageId = "ModuleInsert";
+            this.ExportModulesDrop.ScreenTip = "Export Module";
+            this.ExportModulesDrop.ShowImage = true;
+            this.ExportModulesDrop.ShowItemImage = false;
+            this.ExportModulesDrop.SizeString = "The really long name of a module";
+            this.ExportModulesDrop.SuperTip = "Choose one of the Basic Modules from the same directory as this workbook to impor" +
+    "t.";
+            this.ExportModulesDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ExportModulesDrop_SelectionChanged);
+            // 
+            // ExportClassesDrop
+            // 
+            this.ExportClassesDrop.Label = "Classes";
+            this.ExportClassesDrop.Name = "ExportClassesDrop";
+            this.ExportClassesDrop.OfficeImageId = "ComAddInsDialog";
+            this.ExportClassesDrop.ScreenTip = "Export Class";
+            this.ExportClassesDrop.ShowImage = true;
+            this.ExportClassesDrop.ShowItemImage = false;
+            this.ExportClassesDrop.SizeString = "The really long name of a module";
+            this.ExportClassesDrop.SuperTip = "Choose one of the Class Modules from the same directory as this workbook to impor" +
+    "t.";
+            this.ExportClassesDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ExportClassesDrop_SelectionChanged);
+            // 
+            // ExportFormsDrop
+            // 
+            this.ExportFormsDrop.Label = "Forms";
+            this.ExportFormsDrop.Name = "ExportFormsDrop";
+            this.ExportFormsDrop.OfficeImageId = "FormPublish";
+            this.ExportFormsDrop.ScreenTip = "Export Form";
+            this.ExportFormsDrop.ShowImage = true;
+            this.ExportFormsDrop.ShowItemImage = false;
+            this.ExportFormsDrop.SizeString = "The really long name of a module";
+            this.ExportFormsDrop.SuperTip = "Choose one of the Form Modules from the same directory as this workbook to import" +
+    ".";
+            this.ExportFormsDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ExportFormsDrop_SelectionChanged);
+            // 
+            // ImportAllBtn
+            // 
+            this.ImportAllBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.ImportAllBtn.Label = "Import All";
+            this.ImportAllBtn.Name = "ImportAllBtn";
+            this.ImportAllBtn.OfficeImageId = "Import";
+            this.ImportAllBtn.ScreenTip = "Import All";
+            this.ImportAllBtn.ShowImage = true;
+            this.ImportAllBtn.SuperTip = "Import all VB files from the same directory as this workbook.";
+            this.ImportAllBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ImportAllBtn_Click);
             // 
             // BrowseBtn
             // 
             this.BrowseBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
-            this.BrowseBtn.Label = "Browse";
+            this.BrowseBtn.Label = "From Other Source";
             this.BrowseBtn.Name = "BrowseBtn";
             this.BrowseBtn.OfficeImageId = "BrowseBackgroundImage";
-            this.BrowseBtn.ScreenTip = "From File";
+            this.BrowseBtn.ScreenTip = "Import VB File From Other Source";
             this.BrowseBtn.ShowImage = true;
             this.BrowseBtn.SuperTip = "Import modules from your computer or from other computers you\'re connected to.";
             this.BrowseBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.BrowseBtn_Click);
             // 
-            // ModulesDrop
+            // ExportAllBtn
             // 
-            this.ModulesDrop.Label = "Modules";
-            this.ModulesDrop.Name = "ModulesDrop";
-            this.ModulesDrop.OfficeImageId = "ModuleInsert";
-            this.ModulesDrop.ShowImage = true;
-            this.ModulesDrop.ShowItemImage = false;
-            this.ModulesDrop.SizeString = "The really long name of a module";
-            this.ModulesDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ModulesDrop_SelectionChanged);
+            this.ExportAllBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.ExportAllBtn.Label = "Export All";
+            this.ExportAllBtn.Name = "ExportAllBtn";
+            this.ExportAllBtn.OfficeImageId = "Export";
+            this.ExportAllBtn.ScreenTip = "Export All";
+            this.ExportAllBtn.ShowImage = true;
+            this.ExportAllBtn.SuperTip = "Export all VB files in this workbook to its containing directory";
+            this.ExportAllBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ExportAllBtn_Click);
             // 
-            // ClassesDrop
+            // RefreshGrp
             // 
-            this.ClassesDrop.Label = "Classes";
-            this.ClassesDrop.Name = "ClassesDrop";
-            this.ClassesDrop.OfficeImageId = "ComAddInsDialog";
-            this.ClassesDrop.ShowImage = true;
-            this.ClassesDrop.ShowItemImage = false;
-            this.ClassesDrop.SizeString = "The really long name of a module";
-            this.ClassesDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ClassesDrop_SelectionChanged);
+            this.RefreshGrp.Items.Add(this.RefreshBtn);
+            this.RefreshGrp.Name = "RefreshGrp";
             // 
-            // FormsDrop
+            // RefreshBtn
             // 
-            this.FormsDrop.Label = "Forms";
-            this.FormsDrop.Name = "FormsDrop";
-            this.FormsDrop.OfficeImageId = "FormPublish";
-            this.FormsDrop.ShowImage = true;
-            this.FormsDrop.ShowItemImage = false;
-            this.FormsDrop.SizeString = "The really long name of a module";
-            this.FormsDrop.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.FormsDrop_SelectionChanged);
+            this.RefreshBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.RefreshBtn.Label = "Refresh All";
+            this.RefreshBtn.Name = "RefreshBtn";
+            this.RefreshBtn.OfficeImageId = "RefreshAll";
+            this.RefreshBtn.ScreenTip = "Refresh All Drop Downs";
+            this.RefreshBtn.ShowImage = true;
+            this.RefreshBtn.SuperTip = "Refresh the import drop downs with VB files in this workbook\'s directory.  Refres" +
+    "h the export drop downs with this workbook\'s VB modules.";
+            this.RefreshBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.RefreshBtn_Click);
             // 
             // MainRibbon
             // 
@@ -102,8 +217,12 @@
             this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.MainRibbon_Load);
             this.ModLoaderTab.ResumeLayout(false);
             this.ModLoaderTab.PerformLayout();
-            this.MainGrp.ResumeLayout(false);
-            this.MainGrp.PerformLayout();
+            this.ImportGrp.ResumeLayout(false);
+            this.ImportGrp.PerformLayout();
+            this.ExportGrp.ResumeLayout(false);
+            this.ExportGrp.PerformLayout();
+            this.RefreshGrp.ResumeLayout(false);
+            this.RefreshGrp.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -111,11 +230,20 @@
         #endregion
 
         internal Microsoft.Office.Tools.Ribbon.RibbonTab ModLoaderTab;
-        internal Microsoft.Office.Tools.Ribbon.RibbonGroup MainGrp;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup ImportGrp;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton BrowseBtn;
-        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ModulesDrop;
-        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ClassesDrop;
-        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown FormsDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ImportModulesDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ImportClassesDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ImportFormsDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup ExportGrp;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ExportModulesDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ExportClassesDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown ExportFormsDrop;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ImportAllBtn;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ExportAllBtn;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup RefreshGrp;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton RefreshBtn;
     }
 
     partial class ThisRibbonCollection {
--- ModuleLoader/ModuleLoaderVSTO/MainRibbon.cs
+++ ModuleLoader/ModuleLoaderVSTO/MainRibbon.cs
@@ -5,49 +5,62 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Vbe.Interop;
 using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Tools.Ribbon;
 
 namespace ModuleLoader {
 
     public partial class MainRibbon {
+        // HIDDEN FIELDS
+        private Excel.Application _app;
+
         // EVENT HANDLERS
         private void MainRibbon_Load(object sender, RibbonUIEventArgs e) {
-            Globals.ModuleLoader.Application.WorkbookActivate += Application_WorkbookActivate;
+            _app = Globals.ModuleLoader.Application;
+            _app.WorkbookActivate += Application_WorkbookActivate;
+            _app.WorkbookBeforeClose += Application_WorkbookBeforeClose;
         }
-        private void Application_WorkbookActivate(Workbook Wb) {
-            // Enumerate all the VB files in the same directory as this workbook
-            IEnumerable<FileInfo> modules = null;
-            IEnumerable<FileInfo> classes = null;
-            IEnumerable<FileInfo> forms = null;
-            if (Wb.Path != "") {
-                DirectoryInfo folder = new DirectoryInfo(Wb.Path);
-                modules = folder.EnumerateFiles("*.bas");
-                classes = folder.EnumerateFiles("*.cls");
-                forms = folder.EnumerateFiles("*.frm");
-            }
 
-            // Add their names to the appropriate menus
-            replaceDropDownItems(ModulesDrop, modules, "ModuleInsert");
-            replaceDropDownItems(ClassesDrop, classes, "ComAddInsDialog");
-            replaceDropDownItems(FormsDrop, forms, "FormPublish");
+        private void Application_WorkbookBeforeClose(Workbook Wb, ref bool Cancel) {
+            if (_app.Workbooks.Count == 1)
+                refreshImportItems();
         }
-        private void ModulesDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
-            FileInfo moduleFile = ModulesDrop.SelectedItem.Tag as FileInfo;
-            if (moduleFile != null)
-                doImport(moduleFile);
-            ModulesDrop.SelectedItemIndex = 0;
+
+        private void Application_WorkbookActivate(Workbook Wb) {
+            refreshImportItems(Wb.Path);
         }
-        private void ClassesDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
-            FileInfo classFile = ClassesDrop.SelectedItem.Tag as FileInfo;
-            if (classFile != null)
-                doImport(classFile);
-            ClassesDrop.SelectedItemIndex = 0;
+        private void ImportAllBtn_Click(object sender, RibbonControlEventArgs e) {
+
         }
-        private void FormsDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
-            FileInfo formFile = FormsDrop.SelectedItem.Tag as FileInfo;
-            if (formFile != null)
-                doImport(formFile);
-            FormsDrop.SelectedItemIndex = 0;
+        private void ImportModulesDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
+            RibbonDropDownItem item = ImportModulesDrop.SelectedItem;
+            FileInfo moduleFile = item.Tag as FileInfo;
+            if (moduleFile != null) {
+                bool success = doImport(moduleFile);
+                if (!success)
+                    ImportModulesDrop.Items.Remove(item);
+            }
+            ImportModulesDrop.SelectedItemIndex = 0;
+        }
+        private void ImportClassesDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
+            RibbonDropDownItem item = ImportClassesDrop.SelectedItem;
+            FileInfo classFile = item.Tag as FileInfo;
+            if (classFile != null) {
+                bool success = doImport(classFile);
+                if (!success)
+                    ImportClassesDrop.Items.Remove(item);
+            }
+            ImportClassesDrop.SelectedItemIndex = 0;
+        }
+        private void ImportFormsDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
+            RibbonDropDownItem item = ImportFormsDrop.SelectedItem;
+            FileInfo formFile = item.Tag as FileInfo;
+            if (formFile != null) {
+                bool success = doImport(formFile);
+                if (!success)
+                    ImportFormsDrop.Items.Remove(item);
+            }
+            ImportFormsDrop.SelectedItemIndex = 0;
         }
         private void BrowseBtn_Click(object sender, RibbonControlEventArgs e) {
             // Show an Open File dialog
@@ -68,54 +81,139 @@ namespace ModuleLoader {
                 doImport(f);
             }
         }
+        private void ExportAllBtn_Click(object sender, RibbonControlEventArgs e) {
+
+        }
+        private void ExportModulesDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
+
+        }
+        private void ExportClassesDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
+
+        }
+        private void ExportFormsDrop_SelectionChanged(object sender, RibbonControlEventArgs e) {
+
+        }
+        private void RefreshBtn_Click(object sender, RibbonControlEventArgs e) {
+            string path = Globals.ModuleLoader.Application.ActiveWorkbook.Path;
+            if (path != "")
+                refreshImportItems(path);
+        }
 
         // HELPER FUNCTIONS
-        private void replaceDropDownItems(RibbonDropDown dropDown, IEnumerable<FileInfo> files, string officeImgId) {
-            // No matter what, add a placeholder item to the drop down
+        private void refreshImportItems(string path = "") {
+            // Enumerate all the VB files in the same directory as this workbook and add them to the import drop downs
+            DirectoryInfo folder = (path == "" ? null : new DirectoryInfo(path));
+            refreshDropDownItems(ImportModulesDrop, folder, "*.bas", "ModuleInsert");
+            refreshDropDownItems(ImportClassesDrop, folder, "*.cls", "ComAddInsDialog");
+            refreshDropDownItems(ImportFormsDrop, folder, "*.frm", "FormPublish");
+        }
+        private void refreshDropDownItems(RibbonDropDown dropDown, DirectoryInfo folder, string searchPattern, string officeImgId) {
+            // No matter what, clear the drop down's items and add a placeholder item
             IList<RibbonDropDownItem> items = dropDown.Items;
             items.Clear();
             RibbonDropDownItem item = Factory.CreateRibbonDropDownItem();
             item.Label = "Select a File...";
             items.Add(item);
 
-            // If the given list contains some files, then add their names to the drop down
-            if (files?.Count() > 0) {
-                foreach (FileInfo m in files) {
-                    item = Factory.CreateRibbonDropDownItem();
-                    item.Label = Path.GetFileName(m.Name);
-                    item.OfficeImageId = officeImgId;
-                    item.Tag = m;
-                    items.Add(item);
-                }
+            // If no folder was provided, or it contains no files matching the given search pattern, then just return
+            if (folder == null)
+                return;
+            IEnumerable<FileInfo> files = folder.EnumerateFiles(searchPattern);
+            if (files.Count() == 0)
+                return;
+            
+            // Otherwise, add the VB file names to the given drop down
+            foreach (FileInfo m in files) {
+                item = Factory.CreateRibbonDropDownItem();
+                item.Label = Path.GetFileName(m.Name);
+                item.OfficeImageId = officeImgId;
+                item.Tag = m;
+                items.Add(item);
             }
         }
-        private void doImport(FileInfo module) {
-            // Check if the selected module has already been imported to this workbook
-            string name = Path.GetFileNameWithoutExtension(module.Name);
-            VBComponents currModules = Globals.ModuleLoader.Application.ActiveWorkbook.VBProject.VBComponents;
+        private bool doImport(FileInfo module) {
+            // Try to get the VB name of this module
+            // Inform the user if the file could not be found or could not be loaded
+            string name = "";
+            try {
+                name = getModuleName(module.FullName);
+            }
+            catch (FileNotFoundException) {
+                MessageBox.Show(
+                    $"{module.FullName} could not be found.  Is it possible that the file was moved or renamed?\n\nYou can click '{RefreshBtn.Label}' to refresh the list of available VB files.",
+                    "ModuleLoader", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return false;
+            }
+            catch (FileLoadException) {
+                MessageBox.Show(
+                    $"{module.FullName} could not be loaded.  If you edited this file, make sure that there is a line similar to 'Attribute VB_Name \"SomeName\"' above the actual code.",
+                    "ModuleLoader", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return true;
+            }
+
+            // Check if a module with this name already exists in this workbook
+            VBComponents currModules = _app.ActiveWorkbook.VBProject.VBComponents;
             VBComponent vbc = null;
             try {
                 vbc = currModules.Item(name);
             }
-            catch (IndexOutOfRangeException e) { }
+            catch (IndexOutOfRangeException) { }
 
-            // If so, ask the user if they want to re-import
+            // If so, ask the user if they want to replace it
             bool import = false;
             if (vbc == null)
                 import = true;
             else {
-                DialogResult result = MessageBox.Show("A module with this name already exists in this workbook.\nDo you want to overwrite it?",
-                                                      "Module Loader", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+                DialogResult result = MessageBox.Show(
+                    $"A module with the name {name} already exists in this workbook.\nDo you want to replace it?",
+                    "Module Loader", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
                 if (result == DialogResult.Yes) {
                     currModules.Remove(vbc);
                     import = true;
                 }
+                else if (result== DialogResult.No)
+                    return true;
             }
 
             // If the import is set to continue, then do it!
-            if (import)
+            // Report any errors to the user
+            bool success = false;
+            if (import) {
                 currModules.Import(module.FullName);
+                success = true;
+            }
+            return success;
         }
+        private string getModuleName(string path) {
+            string name = "";
+
+            // Read the file, looking for the "Attribute VB_Name" line
+            string line;
+            bool nameLineFound = false;
+            using (StreamReader file = new StreamReader(path)) {
+                while ((line = file.ReadLine()) != null) {
+                    nameLineFound = (line.Contains("Attribute VB_Name"));
+                    if (!nameLineFound)
+                        continue;
+
+                    string[] parts = line.Split('\"');
+                    if (parts.Count() == 3) {
+                        name = parts[1];
+                        break;
+                    }
+                    else
+                        throw new FileLoadException($"{path} had a line with 'Attribute VB_Name' but no name.  What gives?", path);
+                }
+            }
+
+            // If everything went well, return the Module's name
+            // If the "Attribute VB_Name" line was not found, then this file is invalid so throw an exception
+            if (nameLineFound)
+                return name;
+            else
+                throw new FileLoadException($"{path} did not have a line with 'Attribute VB_Name'.?", path);
+        }
+
     }
 
 }
